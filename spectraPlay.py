bb47983283de18bd339eb9a93996bd1a545fe1fe
zoezawol <30603950+zoezawol@users.noreply.github.com>
2025-04-16 17:26:32 -0700

    updates to account for differences in seismic vs infrasound data
    added some 'if' statements to properly read infrasound data in desired paths/folders based on file/station format and fitted spectra y-axis to the data type's max frequency (150 Hz for seismic and currently 60 Hz for infrasound - can only display 0.5*sampling rate)

--- spectraPlay.py
+++ spectraPlay.py
@@ -6,8 +6,20 @@ import os
 import waveformUtils
 
 # function to plot spectrogram
-def plot_spectrogram(S, t1, t2):
+def plot_spectrogram(S, t1, t2, stas):
     """Function to plot Waveforms, RSAM, and Spectrogram"""
+
+    S.merge(method=1)  # merge traces to avoid duplicates
+
+    # set station info
+    nowsta = stas[0]
+    network, station, location, channel = nowsta.split('.')
+    if network == "ZE":
+        maxFreq = 150
+    elif network == "ZD":
+        maxFreq = 60 # edit value if resample changes
+    else:
+        raise ValueError(f"Unknown network code: {network}")
 
     # f, spectraF, spectraG, sttimes, tvec, RSAM = waveformUtils.plotSpectraTime(S, minfreq=1, maxfreq=100, winlength=7200, step=7200,
     #                                                                            spacing=2, ampScalar=3, normalize=True,
@@ -17,9 +29,7 @@ def plot_spectrogram(S, t1, t2):
     # f.show()
     # remember to also return f when wanting this plot
 
-    S.merge(method=1)  # merge traces to avoid duplicates
-
-    f1 = waveformUtils.multiDaySpectrogram(S, averageLength=3600, fftLength=60, minFreq=0.5, maxFreq=150, cmap='turbo',
+    f1 = waveformUtils.multiDaySpectrogram(S, averageLength=3600, fftLength=60, minFreq=0.5, maxFreq=maxFreq, cmap='turbo',
                                            dateLimits=None, vmin=0.4, vmax=0.9, plotAverage=True)
 
     plt.figure()
@@ -37,22 +47,28 @@ if __name__ == "__main__":
     # setting up parameters for standalone use
 
     # define start and end time for the data range
-    start_date = UTCDateTime(2023, 7, 23)  # Start time
-    end_date = UTCDateTime(2023, 7, 24)    # End time
+    start_date = UTCDateTime(2024, 7, 9)  # Start time
+    end_date = UTCDateTime(2024, 7, 10)    # End time
     delta = 86400  # 1 day in seconds
 
     stas = [2301, 2302, 2303, 2304, 2305, 2306, 2307, 2308, 2309, 2310, 2311, 2312, 2313, 2314, 2315, 2316, 
             'G2301', 'G2302', 'G2303', 'G2304', 'G2305', 'G2306', 'G2307', 'G2308', 'G2309', 'G2310', 'G2311',
             'G2312', 'G2313', 'G2314', 'G2315', 'G2316'] 
-    stas = ['ZE.2304..GP1']  # station IDs
+    stas = ['ZE.2410..GPZ'] # 3C station IDs
+    #stas = ['ZD.G2411.01.HDF'] # GEM station IDs
 
     # set station info
     nowsta = stas[0]
     network, station, location, channel = nowsta.split('.')
-    print(f"Station: {nowsta}")
 
     # define path where .mseed files are stored
-    path = f'C:/Users/zzawol/Documents/seismic-data-iris/seismic_data/NO{station}/{channel}'
+    print(f"Station: {nowsta}")
+    if network == "ZE":
+        path = f'C:/Users/zzawol/Documents/iris-data/seismic_data/NO{station}/{channel}'
+    elif network == "ZD":
+        path = f'C:/Users/zzawol/Documents/iris-data/infrasound_data/{station}'
+    else:
+        raise ValueError(f"Unknown network code: {network}")
 
     # check if the path exists, create if not
     if not os.path.exists(path):
@@ -61,9 +77,12 @@ if __name__ == "__main__":
     else:
         print(f"Path '{path}' already exists.")
 
-    file_pattern = f'{path}/{network}.{station}.*.{channel}*.mseed'
+    file_pattern = f'{path}/{network}.{station}.{location}.{channel}*.mseed'
 
     # read and run plot
     end = end_date + (6*60)
     S = obspy.read(file_pattern, starttime=start_date, endtime=end)
-    plot_spectrogram(S, t1=start_date, t2=end)
+    plot_spectrogram(S, t1=start_date, t2=end, stas=stas)
+
+# run in jupyter by: %run spectraPlay.py
+# or run in terminal by: python spectraPlay.py
