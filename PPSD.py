366b5a5ba4171ae7fea8040cd56b682978bc8466
zoezawol <30603950+zoezawol@users.noreply.github.com>
2025-04-16 17:22:10 -0700

    updates to account for differences in seismic vs infrasound data
    added some 'if' statements to properly read infrasound data in desired paths/folders based on file/station format, read infrasound metadata (different from seismic), and resample rate (120) for PSD calculations. also changed PPSD plot's y-axis to (-200,15,1.0) dB from (-200,50,1.0) to view infrasound's higher amplitude range

--- PPSD.py
+++ PPSD.py
@@ -11,16 +11,31 @@ from obspy.clients import fdsn
 from fractions import Fraction
 
 # function to plot PPSD and temporal plots
-def plot_ppsd(S, inv, t1, t2, user_values):
+def plot_ppsd(S, t1, t2, stas, user_values):
     """Function to calculate and plot PPSD and Temporal Plot"""
     S.merge(method=1)  # merge traces to avoid duplicates
-    S.resample(300)  # resample
+    # define path where .mseed files are stored
+
+    # set station info
+    nowsta = stas[0]
+    network, station, location, channel = nowsta.split('.')
+    if network == "ZE":
+        S.resample(300) # resample
+        client = Client("IRISPH5", timeout=600) # 3C data
+    elif network == "ZD":
+        S.resample(120) # resample
+        client = Client("IRIS", timeout=600) # GEM data
+    else:
+        raise ValueError(f"Unknown network code: {network}")
+
+    # metadata    
+    inv = client.get_stations(network=network, station=station, location=location, channel=channel, level='response')
 
     # initialize PPSD object
     ppsd = None
     for tr in S:
         if ppsd is None:
-            ppsd = PPSD(tr.stats, metadata=inv, period_limits=(1/150, 10.0)) # if xaxis_frequency=False, set period_lim=(1/150,10)
+            ppsd = PPSD(tr.stats, metadata=inv, db_bins=(-200,15,1.0),period_limits=(1/150, 10.0)) # if xaxis_frequency=False, set period_lim=(1/150,10)
         ppsd.add(tr)
         print('Trace added to PPSD')
 
@@ -55,35 +70,51 @@ if __name__ == '__main__':
     stas = [2301, 2302, 2303, 2304, 2305, 2306, 2307, 2308, 2309, 2310, 2311, 2312, 2313, 2314, 2315, 2316, 
         'G2301', 'G2302', 'G2303', 'G2304', 'G2305', 'G2306', 'G2307', 'G2308', 'G2309', 'G2310', 'G2311',
         'G2312', 'G2313', 'G2314', 'G2315', 'G2316'] 
-    stas = ['ZE.2304..GP1'] # stadion IDs
+    stas = ['ZE.2304..GP1'] # 3C station IDs
+    #stas = ['ZD.G2411.01.HDF'] # GEM station IDs
 
     # set station info
     nowsta = stas[0]
     network, station, location, channel = nowsta.split('.')
 
-    # build path based on station/channel
-    path = f'C:/Users/zzawol/Documents/seismic-data-iris/seismic_data/NO{station}/{channel}'
+
+    # can probably delete the whole client section here, it's in main function - same for spectraPlay?
+    # can add path part to inside main function?
+
+    # define path where .mseed files are stored
+    if network == "ZE":
+        path = f'C:/Users/zzawol/Documents/iris-data/seismic_data/NO{station}/{channel}'
+        client = Client("IRISPH5", timeout=600) # 3C metadata
+    elif network == "ZD":
+        path = f'C:/Users/zzawol/Documents/iris-data/infrasound_data/{station}'
+        client = Client("IRIS", timeout=600) # GEM metadata        ## not "IRISDMC" ?
+    else:
+        raise ValueError(f"Unknown network code: {network}")
+    
+    # create inventory for metadata  
+    inv = client.get_stations(network=network, station=station, location=location, channel=channel, level='response')
+
+    # check if the path exists, create if not
     if not os.path.exists(path):
         print(f"Path '{path}' does not exist. Creating directory...")
         os.makedirs(path, exist_ok=True)
     else:
         print(f"Path '{path}' already exists.")
 
-    file_pattern = f'{path}/{network}.{station}.*.{channel}*.mseed'
+    file_pattern = f'{path}/{network}.{station}.{location}.{channel}*.mseed'
 
     # read waveform
     S = read(file_pattern, starttime=start_date, endtime=end_date)
-
-    # get metadata
-    client = Client("IRISPH5", timeout=600)
-    inv = client.get_stations(network=network, station=station, location=location, channel=channel, level='response')
 
     # set frequency values for binning
     user_freqs = [120, 70, 20] # default frequencies in Hz
     user_periods = [1 / freq for freq in user_freqs] # convert to default periods for temporal plot
 
     # call function
-    plot_ppsd(S, inv, start_date, end_date, user_values=user_periods)
+    plot_ppsd(S, start_date, end_date, stas, user_values=user_periods)
+
+# run in jupyter by: %run PPSD.py
+# or run in terminal by: python PPSD.py
 
 
 
